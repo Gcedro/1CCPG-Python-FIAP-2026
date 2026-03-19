72aa8cdce6f015565c5a76e94a90de430c7577f5
Gcedro <gcedro0408@outlook.com>
2026-03-19 11:42:28 -0300

    aula 02 variáveis

---
+++ aula02-variáveis-operadores/aula02-variáveis-operadores.py
@@ -0,0 +1,28 @@
+print("Olá Mundo")
+
+print("7+4")
+print(7+4)
+print("7" + "4") #concatenando strings
+
+#comentário de 1 linha
+'''
+   Comentários de 
+   múltiplas 
+   linhas
+'''
+
+#variáveis
+nome = "Guilherme" #str
+idade = 17 #int
+peso = 51 #float
+
+print("oiii \n",nome, idade, peso)
+print(f"Olá, {nome}!!!")
+
+# input -- simulação de um foms no cmd
+nome = input("Digite o meu nome:")
+idade = int(input("Digite sua idade:"))
+peso = input("Digite o seu peso:")
+
+print(nome, idade, peso)
+print(idade+1)
