c5c20c995755ec7acb186e1f3d4d3c909089d57a
Gcedro <gcedro0408@outlook.com>
2026-04-09 11:39:01 -0300

    aula 3 -funções e condicionais

---
+++ Aula-03-modulos-condicionais/aula03e-match-case.py
@@ -0,0 +1,14 @@
+#escolha do usuário
+#0 --> sair do programa
+#1 --> entrar no programa
+# ---> erro!
+
+escolha_usuario = 1123123
+
+match escolha_usuario:
+    case 0:
+        print("sair do programa")
+    case 1: 
+        print("entrar no programa")
+    case _:
+        print("Erro!")
