280ef23acd9bbd0a4c166d6e138c37a2e9e167d7
Gcedro <gcedro0408@outlook.com>
2026-08-06 11:48:12 -0300

    aula06

---
+++ aula-06-api/exs01_API.py
@@ -0,0 +1,12 @@
+while True:
+    print("Olá Mundo!")
+
+    resposta = input("Deseja ver a mensagem novamente? (S/N)");
+
+    if resposta == "SIM":
+        True
+    else: 
+        print("FIM")
+
+    
+
